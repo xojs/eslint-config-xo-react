d64bc90af2ac5d17249334369ea83cdc7eb12b8b
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-25 01:00:05 +0700

    ES2015ify tests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,11 @@
-'use strict';
-var isPlainObj = require('is-plain-obj');
-var tempWrite = require('temp-write');
-var test = require('ava');
-var eslint = require('eslint');
-var conf = require('../');
+import isPlainObj from 'is-plain-obj';
+import tempWrite from 'temp-write';
+import test from 'ava';
+import eslint from 'eslint';
+import conf from '../';
 
 function runEslint(str) {
-	var linter = new eslint.CLIEngine({
+	const linter = new eslint.CLIEngine({
 		useEslintrc: false,
 		configFile: tempWrite.sync(JSON.stringify(conf))
 	});
@@ -14,11 +13,11 @@ function runEslint(str) {
 	return linter.executeOnText(str).results[0].messages;
 }
 
-test(function (t) {
+test(t => {
 	t.true(isPlainObj(conf));
 	t.true(isPlainObj(conf.rules));
 
-	var errors = runEslint('var app = <div className="foo">Unicorn</div>');
+	const errors = runEslint('var app = <div className="foo">Unicorn</div>');
 	t.is(errors[0].ruleId, 'react/react-in-jsx-scope');
 
 	t.end();
