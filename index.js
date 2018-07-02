b42e95ea1de942685293981d143cdfe19abf7e3b
Sindre Sorhus <sindresorhus@gmail.com>
2018-07-02 12:43:10 +0700

    Add `react/no-unsafe` rule
    https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unsafe.md

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ module.exports = {
 		'react/no-this-in-sfc': 'error',
 		'react/no-unescaped-entities': 'error',
 		'react/no-unknown-property': 'error',
+		'react/no-unsafe': 'error',
 		'react/no-unused-prop-types': 'error',
 		'react/no-unused-state': 'error',
 		'react/no-will-update-set-state': 'error',
