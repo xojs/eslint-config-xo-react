3b49144493630b8a4d2944ee2247fd69e2cd9219
Sindre Sorhus <sindresorhus@gmail.com>
2021-04-03 02:07:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -72,12 +72,15 @@ module.exports = {
 			'never'
 		],
 		'react/static-property-placement': 'error',
-		'react/style-prop-object': ['error', {
-			allow: [
-				// This allows react-intl’s <FormattedNumber value={0.42} style='percent' />
-				'FormattedNumber'
-			]
-		}],
+		'react/style-prop-object': [
+			'error',
+			{
+				allow: [
+					// This allows react-intl’s `<FormattedNumber value={0.42} style='percent'/>`.
+					'FormattedNumber'
+				]
+			}
+		],
 		'react/void-dom-elements-no-children': 'error',
 		'react/jsx-boolean-value': 'error',
 		'react/jsx-closing-bracket-location': [
