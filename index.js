71496a442ef4fe5c905032d6d29039dc98fc6960
Bertrand Marron <bertrand.marron@gmail.com>
2021-04-02 12:35:18 +0200

    Allow react-intl’s usage of style prop

--- index.js
+++ index.js
@@ -72,7 +72,12 @@ module.exports = {
 			'never'
 		],
 		'react/static-property-placement': 'error',
-		'react/style-prop-object': 'error',
+		'react/style-prop-object': ['error', {
+			allow: [
+				// This allows react-intl’s <FormattedNumber value={0.42} style='percent' />
+				'FormattedNumber'
+			]
+		}],
 		'react/void-dom-elements-no-children': 'error',
 		'react/jsx-boolean-value': 'error',
 		'react/jsx-closing-bracket-location': [
