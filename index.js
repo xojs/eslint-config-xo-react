3e8744687a57c26d7eb1f929f2be5c944e01e916
Sindre Sorhus <sindresorhus@gmail.com>
2022-01-09 16:56:23 +0700

    Add `react/no-invalid-html-attribute` rule
    https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-invalid-html-attribute.md

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ module.exports = {
 		'react/no-did-update-set-state': 'error',
 		'react/no-direct-mutation-state': 'error',
 		'react/no-find-dom-node': 'error',
+		'react/no-invalid-html-attribute': 'error',
 		'react/no-is-mounted': 'error',
 		'react/no-namespace': 'error',
 		'react/no-redundant-should-component-update': 'error',
